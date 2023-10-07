e22fdbc3e8a4c2bb46e0e4aae8da85425e44f67c
Stephen Halter <halter73@gmail.com>
2023-10-06 19:16:33 -0700

    dotnet new blazor -au Individual -ai -int Auto -e --use-program-main
    https://github.com/dotnet/aspnetcore/tree/051c204125f437db02f23894ae1bd2b3022aa947

---
+++ auth-ai-intAuto-e-upm/auth-ai-intAuto-e-upm.Client/PersistentAuthenticationStateProvider.cs
@@ -0,0 +1,36 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace auth_ai_intAuto_e_upm.Client;
+
+// This is a client-side AuthenticationStateProvider that determines the user's authentication state by
+// looking for data persisted in the page when it was rendered on the server. This authentication state will
+// be fixed for the lifetime of the WebAssembly application. So, if the user needs to log in or out, a full
+// page reload is required.
+//
+// This only provides a user name and email for display purposes. It does not actually include any tokens
+// that authenticate to the server when making subsequent requests. That works separately using a
+// cookie that will be included on HttpClient requests to the server.
+public class PersistentAuthenticationStateProvider(PersistentComponentState persistentState) : AuthenticationStateProvider
+{
+    private static readonly Task<AuthenticationState> unauthenticatedTask =
+        Task.FromResult(new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity())));
+
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        if (!persistentState.TryTakeFromJson<UserInfo>(nameof(UserInfo), out var userInfo) || userInfo is null)
+        {
+            return unauthenticatedTask;
+        }
+
+        Claim[] claims = [
+            new Claim(ClaimTypes.NameIdentifier, userInfo.UserId),
+            new Claim(ClaimTypes.Name, userInfo.Email),
+            new Claim(ClaimTypes.Email, userInfo.Email) ];
+
+        return Task.FromResult(
+            new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(claims,
+                authenticationType: nameof(PersistentAuthenticationStateProvider)))));
+    }
+}
