7c2937f80fff5e59575a857d5a71067e65241c2d
Stephen Halter <halter73@gmail.com>
2023-10-06 19:15:36 -0700

    dotnet new blazor -au Individual -ai -int None -uld -e --use-program-main
    https://github.com/dotnet/aspnetcore/tree/051c204125f437db02f23894ae1bd2b3022aa947

---
+++ auth-ai-intNone-uld-e-upm/Data/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace auth_ai_intNone_uld_e_upm.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
+
