daed3f3226be8c951ab42bf5c085fb82a32b7dd1
Stephen Halter <halter73@gmail.com>
2023-10-06 19:14:53 -0700

    dotnet new blazor -au Individual -int Server -uld -e
    https://github.com/dotnet/aspnetcore/tree/051c204125f437db02f23894ae1bd2b3022aa947

---
+++ auth-intServer-uld-e/Data/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace auth_intServer_uld_e.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
+
