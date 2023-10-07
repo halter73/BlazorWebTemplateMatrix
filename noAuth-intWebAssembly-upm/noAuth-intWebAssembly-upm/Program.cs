ee56601c01ff35669a46c4c43bf8229d072c2d2d
Stephen Halter <halter73@gmail.com>
2023-10-06 19:17:24 -0700

    dotnet new blazor -au None -int WebAssembly --use-program-main
    https://github.com/dotnet/aspnetcore/tree/051c204125f437db02f23894ae1bd2b3022aa947

---
+++ noAuth-intWebAssembly-upm/noAuth-intWebAssembly-upm/Program.cs
@@ -0,0 +1,41 @@
+using noAuth_intWebAssembly_upm.Client.Pages;
+using noAuth_intWebAssembly_upm.Components;
+
+namespace noAuth_intWebAssembly_upm;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveWebAssemblyComponents();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseWebAssemblyDebugging();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveWebAssemblyRenderMode()
+            .AddAdditionalAssemblies(typeof(Counter).Assembly);
+
+        app.Run();
+    }
+}
